c0f27bc73bdd412cf6c1e227ab0516cae1117f81
Enrico Bartocetti <enrico.bartocetti@studio.unibo.it>
2024-11-04 12:13:55 +0100

    Ex 71: Implemented the filter policy
    Corrected by the tutor

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -1,5 +1,6 @@
 package it.unibo.inner.impl;
 
+import java.util.Arrays;
 import java.util.Iterator;
 
 import it.unibo.inner.api.IterableWithPolicy;
@@ -14,9 +15,32 @@ import it.unibo.inner.api.Predicate;
 public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
 
     private final T[] array;
+    private Predicate<T> policy;
 
+    /**
+     * All the elements of the collection will be considered for the iteration.
+     * @param array Elements to be added to the collection
+     */
     public IterableWithPolicyImpl(final T[] array) {
-        this.array = array;
+        this(
+            array,
+            new Predicate<T>() {
+                @Override
+                public boolean test(T elem) {
+                    return true;
+                }
+            }
+        );
+    }
+
+    /**
+     * Initialize the class so that the iteration skips the elements using the filter selected.
+     * @param array Elements to be added to the collection
+     * @param pred Rule to filter the elements during the iteration
+     */
+    public IterableWithPolicyImpl(final T[] array, final Predicate<T> pred) {
+        this.array = Arrays.copyOf(array, array.length);
+        this.policy = pred;
     }
 
     /**
@@ -32,21 +56,33 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
      */
     @Override
     public void setIterationPolicy(Predicate<T> filter) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setIterationPolicy'");
+        this.policy = filter;
     }
 
     private class PolicyIterator implements Iterator<T> {
-        private int index;
+        private int index = 0;
 
         @Override
         public boolean hasNext() {
-            return this.index < array.length;
+            int i = index;
+
+            while (i < array.length && !policy.test(array[i])) {
+                i++;
+            }
+
+            return i < array.length;
         }
 
         @Override
         public T next() {
-            return array[index++];
+            T elem;
+            elem = array[index++];
+
+            while (!policy.test(elem)) {
+                elem = array[index++];
+            }
+
+            return elem;
         }
 
     }
